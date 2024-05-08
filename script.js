550eccaca2921423fa36254f9f2c27cd6168c385
Rana970 <ranasulaiman.x099@gmail.com>
2024-05-08 16:42:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.add('active');
+}
+
+document.querySelector('#close-navbar').onclick = () => {
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const membersList = document.getElementById('members-list');
+
+    function Reorder_randomly() {
+        for (let i = membersList.children.length; i >= 0; i--) {
+            membersList.appendChild(membersList.children[Math.random() * i | 0]);
+        }
+    }
+
+    Reorder_randomly();
+
+});
